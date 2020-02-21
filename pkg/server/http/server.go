f953370ef89a358433279e22ef753d1c1b0cab80
A.Unger <zyxancf@gmail.com>
2020-02-21 15:35:56 +0100

    wip poc

--- pkg/server/http/server.go
+++ pkg/server/http/server.go
@@ -1,19 +1,37 @@
 package http
 
 import (
-	"encoding/json"
-	"net/http"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net/http/httputil"
+	"net/url"
 
-	"github.com/micro/go-micro/v2/config"
-	"github.com/micro/go-micro/v2/config/source/memory"
-	"github.com/micro/go-plugins/micro/router/v2"
 	"github.com/owncloud/ocis-bridge/pkg/version"
+	ocCrypto "github.com/owncloud/ocis-konnectd/pkg/crypto"
 	svc "github.com/owncloud/ocis-pkg/v2/service/http"
 )
+
+type proxyHandler struct {
+	handler *httputil.ReverseProxy
+}
 
 // Server initializes the http service and server.
 func Server(opts ...Option) (svc.Service, error) {
 	options := newOptions(opts...)
+
+	if err := ocCrypto.GenCert(options.Logger); err != nil {
+		log.Fatal(err)
+	}
+
+	cer, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tlsConf := tls.Config{
+		Certificates: []tls.Certificate{cer},
+	}
 
 	service := svc.NewService(
 		svc.Logger(options.Logger),
@@ -23,151 +41,38 @@ func Server(opts ...Option) (svc.Service, error) {
 		svc.Address(options.Config.HTTP.Addr),
 		svc.Context(options.Context),
 		svc.Flags(options.Flags...),
+		svc.TLSConfig(&tlsConf),
 	)
 
-	// TODO replace hardcoded adresses with service registry lookups
-	routes := []router.Route{
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/.well-known/openid-configuration",
-			},
-			ProxyURL: router.URL{
-				Scheme: "https",
-				Host:   "localhost:9130",
-				Path:   "/.well-known/openid-configuration",
-			},
-			Weight: 2.0,
-			Type:   "proxy",
-		},
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/signin/v1",
-			},
-			ProxyURL: router.URL{
-				Scheme: "https",
-				Host:   "localhost:9130",
-				Path:   "/signin/v1",
-			},
-			Weight: 2.0,
-			Type:   "proxy",
-		},
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/konnect/v1",
-			},
-			ProxyURL: router.URL{
-				Scheme: "https",
-				Host:   "localhost:9130",
-				Path:   "/konnect/v1",
-			},
-			Weight: 2.0,
-			Type:   "proxy",
-		},
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/reva",
-			},
-			ProxyURL: router.URL{
-				Scheme: "http",
-				Host:   "localhost:9140",
-				Path:   "/",
-			},
-			Weight: 2.0,
-			Type:   "proxy",
-		},
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/phoenix/",
-			},
-			ProxyURL: router.URL{
-				Scheme: "http",
-				Host:   "localhost:9100",
-				Path:   "/phoenix/",
-			},
-			Weight: 2.0,
-			Type:   "proxy",
-		},
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/graph/",
-			},
-			ProxyURL: router.URL{
-				Scheme: "http",
-				Host:   "localhost:9120",
-				Path:   "/",
-			},
-			Weight: 2.0,
-			Type:   "proxy",
-		},
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/graph-explorer/",
-			},
-			ProxyURL: router.URL{
-				Scheme: "http",
-				Host:   "localhost:9135",
-				Path:   "/",
-			},
-			Weight: 2.0,
-			Type:   "proxy",
-		},
-		{
-			Request: router.Request{
-				Method: "GET",
-				Host:   "localhost:9200",
-				Path:   "/",
-			},
-			Response: router.Response{
-				StatusCode: 302,
-				Header: map[string]string{
-					"Location": "http://localhost:9200/phoenix/",
-				},
-			},
-			Weight: 1.0,
-		},
-	}
-
-	apiConfig := map[string]interface{}{
-		"api": map[string]interface{}{
-			"routes": routes,
-		},
-	}
-
-	b, _ := json.Marshal(apiConfig)
-	m := memory.NewSource(memory.WithJSON(b))
-	conf, err := config.NewConfig(config.WithSource(m))
-	if err != nil {
-		options.Logger.Fatal().
-			Err(err).
-			Msg("could not parse routes")
-	}
-
-	r := router.NewRouter(router.Config(conf))
-
-	wr := r.Handler()
-	h := wr(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "not found", 404)
-	}))
-
-	service.Handle(
-		"/",
-		h,
-	)
+	service.Handle("/", handler(phoenix))
+	service.Handle("/ocs/", handler(reva))
+	service.Handle("/signin/", handler(konnectd))
+	service.Handle("/konnect/", handler(konnectd))
+	service.Handle("/remote.php/webdav/", handler(reva))
+	service.Handle("/.well-known/openid-configuration", handler(konnectd))
 
 	service.Init()
 	return service, nil
 }
+
+func mustURL(s string) *url.URL {
+	url, err := url.Parse(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return url
+}
+
+func handler(port servicePort) *httputil.ReverseProxy {
+	return httputil.NewSingleHostReverseProxy(mustURL(fmt.Sprintf("http://localhost:%v", port)))
+}
+
+type servicePort string
+
+var (
+	phoenix  servicePort = "9100"
+	konnectd servicePort = "9130"
+	reva     servicePort = "9140"
+	webdav   servicePort = "9115"
+)
